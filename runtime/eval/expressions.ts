d20ef75251fac910333e2201fca78db5a18d8980
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 14:35:11 +0800

    Scripted auto-commit on change (2024-04-30 14:35:11) by gitwatch.sh

--- runtime/eval/expressions.ts
+++ runtime/eval/expressions.ts
@@ -46,5 +46,6 @@ export function eval_assignment(node: AssignmentExpr, env: Environment): Runtime
   if (node.assigne.kind !== "Identifier")
     throw `Invalid LHS inside assignment expr ${JSON.stringify(node.assigne)}`;
 
+  const varname = node.assigne
   return env.assignVar(node.assigne)
 }
