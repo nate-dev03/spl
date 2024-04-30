c4efa027f2f3390e5c899a8583d3b208f6d2b100
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 12:53:36 +0800

    Scripted auto-commit on change (2024-04-30 12:53:36) by gitwatch.sh

--- runtime/environment.ts
+++ runtime/environment.ts
@@ -6,5 +6,8 @@ export default class Environment {
     private parent?: Environment;
     private variables: Map<string, RuntimeVal>;
 
-    constructor(parentENV?: Environment)
+    constructor(parentENV?: Environment) {
+        this.parent = parentENV;
+        this.variables = 
+    }
 }
