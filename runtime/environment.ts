3db9395b0e28826c2eb9a54dbbaf8ede20301c30
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 12:55:00 +0800

    Scripted auto-commit on change (2024-04-30 12:55:00) by gitwatch.sh

--- runtime/environment.ts
+++ runtime/environment.ts
@@ -13,7 +13,7 @@ export default class Environment {
 
     public declareVar(varname: string, value: RuntimeVal): RuntimeVal {
         if (this.variables.has(varname)) {
-            throw "Cannot declare variable ${varname}`
+            throw `Cannot declare variable ${varname}`
         }
     }
 }
