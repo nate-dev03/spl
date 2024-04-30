3bb9842e82939c8a44250b40023724311cb921af
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 14:50:35 +0800

    Scripted auto-commit on change (2024-04-30 14:50:35) by gitwatch.sh

--- main.ts
+++ main.ts
@@ -23,7 +23,8 @@ async function run(filename: string) {
 
   const input = await Deno.readTextFile(filename);
   const program = parser.produceAST(input);
-  const result = evaluate
+  const result = evaluate(program, env);
+  console.log(result);
 }
 
 function repl() {
