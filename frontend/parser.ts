e0f7918a505c4553ac22dee1f9e2e366d1fde84c
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 15:13:40 +0800

    Scripted auto-commit on change (2024-04-30 15:13:40) by gitwatch.sh

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -130,8 +130,7 @@ export default class Parser {
         properties.push({ key, kind: "Property", value: undefined } as Property);
         continue;
       } // Allows shorthand key: pair -> key,
-      else if (this.at().type === TokenType.CloseBrace) {
-        this.eat(); // advance past comma
+      else if (this.at().type === TokenType.CloseBrace) {        
         properties.push({ key, kind: "Property", value: undefined } as Property);
         continue;
       }
