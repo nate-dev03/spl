cd3d3566f5750fe0b9b4c7593d10ce7f95d3712b
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 14:25:47 +0800

    Scripted auto-commit on change (2024-04-30 14:25:47) by gitwatch.sh

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -98,9 +98,9 @@ export default class Parser {
   private parse_expr(): Expr {
     return this.parse_assignment_expr();
   }
-  
+
   private parse_assignment_expr(): Expr {
-    throw new Error("Method not implemented.");
+  
   }
 
   private parse_additive_expr(): Expr {
