81d3003b37174e421c0e9614e47177ccd905d03b
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 15:23:19 +0800

    Scripted auto-commit on change (2024-04-30 15:23:19) by gitwatch.sh

--- runtime/eval/expressions.ts
+++ runtime/eval/expressions.ts
@@ -1,55 +1,80 @@
-import { AssignmentExpr, BinaryExpr, Identifier, ObjectLiteral } from "../../frontend/ast.ts";
+import {
+  AssignmentExpr,
+  BinaryExpr,
+  Identifier,
+  ObjectLiteral,
+} from "../../frontend/ast.ts";
 import Environment from "../environment.ts";
 import { evaluate } from "../interpreter.ts";
-import { NumberVal, RuntimeVal, MK_NULL } from "../values.ts";
+import { MK_NULL, NumberVal, RuntimeVal } from "../values.ts";
 
-function eval_numeric_binary_expr(lhs: NumberVal, rhs: NumberVal, operator: string): NumberVal {
-    let result = 0;
-  
-    if (operator === "+") {
-      result = lhs.value + rhs.value;
-    } else if (operator === "-") {
-      result = lhs.value - rhs.value;
-    } else if (operator === "*") {
-      result = lhs.value * rhs.value;
-    } else if (operator === "/") {
-      if (rhs.value !== 0) {
-        result = lhs.value / rhs.value;
-      } else {
-        console.error("Invalid: division by zero");
-      }
+function eval_numeric_binary_expr(
+  lhs: NumberVal,
+  rhs: NumberVal,
+  operator: string,
+): NumberVal {
+  let result = 0;
+
+  if (operator === "+") {
+    result = lhs.value + rhs.value;
+  } else if (operator === "-") {
+    result = lhs.value - rhs.value;
+  } else if (operator === "*") {
+    result = lhs.value * rhs.value;
+  } else if (operator === "/") {
+    if (rhs.value !== 0) {
+      result = lhs.value / rhs.value;
     } else {
-      result = lhs.value % rhs.value;
+      console.error("Invalid: division by zero");
     }
-  
-    return { value: result, type: "number" };
+  } else {
+    result = lhs.value % rhs.value;
   }
-  
-  export function eval_binary_expr(binop: BinaryExpr, env: Environment): RuntimeVal {
-    const lhs = evaluate(binop.left, env);
-    const rhs = evaluate(binop.right, env);
-  
-    if(lhs.type === "number" && rhs.type === "number") {
-      return eval_numeric_binary_expr(lhs as NumberVal, rhs as NumberVal, binop.operator);
-    }
-  
-    // One or both are null
-    return MK_NULL();
-  }
-  
-export function eval_identifier(ident: Identifier, env: Environment): RuntimeVal {
-    const val = env.lookupVar(ident.symbol);
-    return val;
+
+  return { value: result, type: "number" };
 }
 
-export function eval_assignment(node: AssignmentExpr, env: Environment): RuntimeVal {
-  if (node.assigne.kind !== "Identifier")
+export function eval_binary_expr(
+  binop: BinaryExpr,
+  env: Environment,
+): RuntimeVal {
+  const lhs = evaluate(binop.left, env);
+  const rhs = evaluate(binop.right, env);
+
+  if (lhs.type === "number" && rhs.type === "number") {
+    return eval_numeric_binary_expr(
+      lhs as NumberVal,
+      rhs as NumberVal,
+      binop.operator,
+    );
+  }
+
+  // One or both are null
+  return MK_NULL();
+}
+
+export function eval_identifier(
+  ident: Identifier,
+  env: Environment,
+): RuntimeVal {
+  const val = env.lookupVar(ident.symbol);
+  return val;
+}
+
+export function eval_assignment(
+  node: AssignmentExpr,
+  env: Environment,
+): RuntimeVal {
+  if (node.assigne.kind !== "Identifier") {
     throw `Invalid LHS inside assignment expr ${JSON.stringify(node.assigne)}`;
+  }
 
   const varname = (node.assigne as Identifier).symbol;
   return env.assignVar(varname, evaluate(node.value, env));
 }
 
-export function eval_object_expr(obj: ObjectLiteral, env: Environment): RuntimeVal {
-  
+export function eval_object_expr(
+  obj: ObjectLiteral,
+  env: Environment,
+): RuntimeVal {
 }
