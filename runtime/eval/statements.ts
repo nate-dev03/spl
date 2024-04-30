a114e739c613a9db1b354a6bf008b691a1460acd
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 14:02:45 +0800

    Scripted auto-commit on change (2024-04-30 14:02:44) by gitwatch.sh

--- runtime/eval/statements.ts
+++ runtime/eval/statements.ts
@@ -4,13 +4,15 @@ import { evaluate } from "../interpreter.ts";
 import { MK_NULL, type RuntimeVal } from "../values.ts";
 
 export function eval_program(program: Program, env: Environment): RuntimeVal {
-    let lastEvaluated: RuntimeVal = MK_NULL();
-    for (const stmt of program.body) {
-      lastEvaluated = evaluate(stmt, env);
-    }
-    return lastEvaluated;
+  let lastEvaluated: RuntimeVal = MK_NULL();
+  for (const stmt of program.body) {
+    lastEvaluated = evaluate(stmt, env);
+  }
+  return lastEvaluated;
 }
 
-export function eval_var_declaration(arg0: VarDeclaration, env: Environment): RuntimeVal {
-    
+export function eval_var_declaration(
+  arg0: VarDeclaration,
+  env: Environment,
+): RuntimeVal {
 }
