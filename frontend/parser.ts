729b4ab687892dedbafd53adc37ce0e1af941313
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 15:03:02 +0800

    Scripted auto-commit on change (2024-04-30 15:03:02) by gitwatch.sh

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -112,7 +112,7 @@ export default class Parser {
     return left;
   }
   private parse_object_expr(): Expr {
-    if (this.at().type !== )
+    if (this.at().type !== TokenType.)
   }
 
   private parse_additive_expr(): Expr {
