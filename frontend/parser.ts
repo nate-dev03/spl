b80d04c2a68e5f72a43445e02db5f25c35b95a3c
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 14:27:08 +0800

    Scripted auto-commit on change (2024-04-30 14:27:08) by gitwatch.sh

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -98,7 +98,9 @@ export default class Parser {
   private parse_expr(): Expr {
     const left = this.parse_additive_expr(); // switch this out with objectExpr
 
-    if ()
+    if (this.at().type == TokenType.Equals) {
+      
+    }
   }
 
   private parse_assignment_expr(): Expr {
