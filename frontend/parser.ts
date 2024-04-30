79e10623e4b1ece0ea303d06ae622d97c8be3422
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 15:14:22 +0800

    Scripted auto-commit on change (2024-04-30 15:14:22) by gitwatch.sh

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -136,7 +136,7 @@ export default class Parser {
       }
 
       // { key: val }
-      this.expect(TokenType.Colon, )
+      this.expect(TokenType.Colon, "Missing colon ")
     }
 
     this.expect(TokenType.CloseBrace, "Object literal missing closing brace.");
