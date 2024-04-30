d5e8c90e1c355ebcd35b2200fbe90ea649ec3432
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 13:21:37 +0800

    Scripted auto-commit on change (2024-04-30 13:21:37) by gitwatch.sh

--- main.ts
+++ main.ts
@@ -16,7 +16,8 @@ function repl() {
   const parser = new Parser();
   const env = new Environment();
   env.declareVar("x", MK_NUMBER(100));
-  env.declareVar("") 
+  env.declareVar("true", MK_BOOL(true));
+   
   console.log("\nRepl v0.1");
   while (true) {
     const input = prompt("> ");
