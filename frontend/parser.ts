689abd80c4d80bdf3f0a9f2570a00ad052d1be30
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 15:11:24 +0800

    Scripted auto-commit on change (2024-04-30 15:11:24) by gitwatch.sh

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -122,7 +122,7 @@ export default class Parser {
     const properties = new Array<Property>();
 
     while (this.not_eof() && this.at().type !== TokenType.CloseBrace) {
-      const key = this.expect(TokenType.Identifier, "Object literal key expected.");
+      const key = this.expect(TokenType.Identifier, "Object literal key expected.").value;
     }
 
     this.expect(TokenType.CloseBrace, "Object literal missing closing brace.");
