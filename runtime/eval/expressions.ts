7fa40fc0b12c6c139da716d61a6523ebe2aee7e8
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 13:57:54 +0800

    Scripted auto-commit on change (2024-04-30 13:57:54) by gitwatch.sh

---
+++ runtime/eval/expressions.ts
@@ -0,0 +1,39 @@
+
+function eval_numeric_binary_expr(lhs: NumberVal, rhs: NumberVal, operator: string): NumberVal {
+    let result = 0;
+  
+    if (operator === "+") {
+      result = lhs.value + rhs.value;
+    } else if (operator === "-") {
+      result = lhs.value - rhs.value;
+    } else if (operator === "*") {
+      result = lhs.value * rhs.value;
+    } else if (operator === "/") {
+      if (rhs.value !== 0) {
+        result = lhs.value / rhs.value;
+      } else {
+        console.error("Invalid: division by zero");
+      }
+    } else {
+      result = lhs.value % rhs.value;
+    }
+  
+    return { value: result, type: "number" };
+  }
+  
+  function eval_binary_expr(binop: BinaryExpr, env: Environment): RuntimeVal {
+    const lhs = evaluate(binop.left, env);
+    const rhs = evaluate(binop.right, env);
+  
+    if(lhs.type === "number" && rhs.type === "number") {
+      return eval_numeric_binary_expr(lhs as NumberVal, rhs as NumberVal, binop.operator);
+    }
+  
+    // One or both are null
+    return MK_NULL();
+  }
+  
+  function eval_identifier(ident: Identifier, env: Environment): RuntimeVal {
+    const val = env.lookupVar(ident.symbol);
+    return val;
+  }
