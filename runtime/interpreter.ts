a5f873cd8d14a4801c111519d3b976daea0e583f
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 13:14:18 +0800

    Scripted auto-commit on change (2024-04-30 13:14:18) by gitwatch.sh

--- runtime/interpreter.ts
+++ runtime/interpreter.ts
@@ -57,7 +57,7 @@ export function evaluate(astNode: Stmt, env: Environment): RuntimeVal {
         value: ((astNode as NumericLiteral).value),
       } as NumberVal;
     case "NullLiteral":
-      return { type: "null", value: "null" } as NullVal;
+      return MK_NULL();
     case "Identifier":
       return eval_identifier(astNode as Identifier, env);
     case "BinaryExpr":
