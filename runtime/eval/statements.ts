62b4482da55e38ebf15542722488fc02d7472507
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 13:56:54 +0800

    Scripted auto-commit on change (2024-04-30 13:56:54) by gitwatch.sh

--- runtime/eval/statements.ts
+++ runtime/eval/statements.ts
@@ -1,6 +1,5 @@
 import { Program } from "../../frontend/ast.ts";
 import Environment from "../environment.ts";
-// biome-ignore lint/style/useImportType: <explanation>
 import { MK_NULL, RuntimeVal } from "../values.ts";
 
 export function eval_program(program: Program, env: Environment): RuntimeVal {
