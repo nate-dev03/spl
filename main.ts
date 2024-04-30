35363122b79379114e892fa498ddf572a35ce8a1
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 14:49:59 +0800

    Scripted auto-commit on change (2024-04-30 14:49:59) by gitwatch.sh

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ async function run(filename: string) {
   env.declareVar("false", MK_BOOL(false), true);
   env.declareVar("null", MK_NULL(), true);
 
-  const 
+  const input = 
 }
 
 function repl() {
