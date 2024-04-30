a46c00ee4bb86f424fd54a1c90e9b74129830de9
nate-dev03 <leinsomar19@gmail.com>
2024-04-30 13:22:04 +0800

    Scripted auto-commit on change (2024-04-30 13:22:04) by gitwatch.sh

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import Parser from "./frontend/parser.ts";
 import Environment from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
-import { MK_NUMBER, type NumberVal } from "./runtime/values.ts";
+import { MK_NULL, MK_NUMBER, type NumberVal } from "./runtime/values.ts";
 
 /**
 const source = await Deno.readTextFile("./test.spl");
@@ -18,7 +18,7 @@ function repl() {
   env.declareVar("x", MK_NUMBER(100));
   env.declareVar("true", MK_BOOL(true));
   env.declareVar("false", MK_BOOL(false));
-   
+  env.declareVar("null", MK_NULL()); 
   console.log("\nRepl v0.1");
   while (true) {
     const input = prompt("> ");
